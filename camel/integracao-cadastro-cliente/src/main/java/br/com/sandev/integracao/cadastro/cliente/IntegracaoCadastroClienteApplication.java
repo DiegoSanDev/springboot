0fc0efaf41410c73b04491eeba25dc943fc67aba
Diego Santos <diegosan.dev@gmail.com>
2022-06-26 12:36:01 -0300

    Cria projeto para realizar integração entre serviços utilizando o Apache Camel

---
+++ camel/integracao-cadastro-cliente/src/main/java/br/com/sandev/integracao/cadastro/cliente/IntegracaoCadastroClienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.sandev.integracao.cadastro.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegracaoCadastroClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegracaoCadastroClienteApplication.class, args);
+	}
+
+}
---
+++ camel/integracao-cadastro-cliente/src/test/java/br/com/sandev/integracao/cadastro/cliente/IntegracaoCadastroClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sandev.integracao.cadastro.cliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracaoCadastroClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
